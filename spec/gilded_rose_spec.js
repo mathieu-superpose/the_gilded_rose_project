efcc8c028fc944d0f9a0b658f2190c56861924a7
Mathieu JOLY <m.joly@live.fr>
2021-02-24 10:55:43 +0100

    update test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,20 +1,35 @@
-var { Shop, Item } = require('../src/js/gilded_rose.js');
+const { Shop, Item } = require('../src/js/gilded_rose.js');
 describe("GildedRose shop manager", function () {
-  var listItems;
+  let listItems = [];
 
   beforeEach(function () {
     listItems = [];
   });
 
-
   it("Baisser de 1 la qualité et sellIn d'item normaux", function () {
     listItems.push(new Item("+5 Dexterity Vest", 10, 20));
+    listItems.push(new Item("Mana Cake", 2, 2));
+
+    listItems.forEach(item=>item.updateQuality());
+
+    const expected = [
+      { sellIn: 9, quality: 19 },
+      { sellIn: 1, quality: 1 }
+    ];
+    expected.forEach(function (testCase, idx) {
+      expect(listItems[idx].quality).toBe(testCase.quality);
+      expect(listItems[idx].sellIn).toBe(testCase.sellIn);
+    });
+  });
+
+  it("Qualité reste à zéro", function () {
+    listItems.push(new Item("+5 Dexterity Vest", 2, 0));
     listItems.push(new Item("Mana Cake", -1, 0));
 
     listItems.forEach(item=>item.updateQuality());
 
-    var expected = [
-      { sellIn: 9, quality: 19 },
+    const expected = [
+      { sellIn: 1, quality: 0 },
       { sellIn: -2, quality: 0 }
     ];
     expected.forEach(function (testCase, idx) {
@@ -30,7 +45,7 @@ describe("GildedRose shop manager", function () {
   //   const gildedRose = new Shop(listItems);
   //   const items = gildedRose.updateQuality();
 
-  //   var expected = [
+  //   const expected = [
   //     { sellIn: 19, quality: 31 },
   //     { sellIn: 19, quality: 31 },
   //   ];
@@ -46,7 +61,7 @@ describe("GildedRose shop manager", function () {
   //   const gildedRose = new Shop(listItems);
   //   const items = gildedRose.updateQuality();
 
-  //   var expected = [
+  //   const expected = [
   //     { sellIn: Infinity, quality: 80 },
   //   ];
   //   expected.forEach(function (testCase, idx) {
